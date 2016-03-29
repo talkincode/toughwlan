3d272bde68fb5751237671c8c33d984ec80178db
jamiesun <jamiesun.net@gmail.com>
2016-03-29 13:33:49 +0800

    add:build dir

---
+++ build/stable/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! -f "/var/toughwlan/data" ];then
+    mkdir -p /var/toughwlan/data
+fi
+
+if [ ! -f "/var/toughwlan/.install" ];then
+    pypy /opt/toughwlan/toughctl --initdb
+    echo "ok" > /var/toughwlan/.install
+    echo "init database ok!"
+fi
+
+exec "$@"
